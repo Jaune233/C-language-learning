5d81b3e25280cf7229eb13cf190402cfef70720e
Jaune233 <145834508+Jaune233@users.noreply.github.com>
2023-10-29 16:28:21 +0800

    Add files via upload

---
+++ 20231029-operator_3.c
@@ -0,0 +1,11 @@
+int main()
+{
+int a=2;
+a=a+2;
+//等同于a+=2;
+int b=3;
+b-=2;
+//等同于b=b-2:
+printf("%d\n",b);
+return 0;
+}
---
+++ 20231029-operator_4.c
@@ -0,0 +1,7 @@
+int main()
+{
+    int arr[10]={0};
+    int sz=sizeof(arr)/sizeof(arr[0]);
+    printf("%d\n",sz);
+    return 0;
+}
