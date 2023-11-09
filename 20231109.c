07690e6329e2fed1aa75ed9dccd9b1b2786cb238
Jaune233 <145834508+Jaune233@users.noreply.github.com>
2023-11-09 16:54:44 +0800

    Add files via upload

---
+++ 20231109.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+void miku() {
+
+	static int a = 1;
+	a++;
+	printf("%d", a);
+}
+
+int main() {
+
+	int i = 0;
+	while (i < 10) {
+
+		miku();
+		i++;
+
+	}
+	return 0;
+}
